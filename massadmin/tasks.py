b1c793b8397ed7ab2400cb3917e117ef46d4b593
Sebastian Wroblewski <swroblewski@motoinsight.com>
2022-11-02 10:22:55 +0100

    MI-1415: Another approach, I can't test it locally anymore

--- massadmin/tasks.py
+++ massadmin/tasks.py
@@ -36,70 +36,6 @@ try:  # Django>=1.9
 except ImportError:
     from django.db.models import get_model
 
-
-# Turns out I might be able to create a fake request object containing just a user object
-# Maybe this will allow for a swift transition, and let me do the thing quicker
-
-# request.__dict__ dumps the request into a json file, maybe it will be enough?
-
-class MassAdminAdminModel(admin.ModelAdmin):
-    def get_form(self, request, obj=None, change=False, **kwargs):
-        """
-        Return a Form class for use in the admin add view. This is used by
-        add_view and change_view.
-        """
-        if "fields" in kwargs:
-            fields = kwargs.pop("fields")
-        else:
-            fields = flatten_fieldsets(self.get_fieldsets(request, obj))
-        excluded = self.get_exclude(request, obj)
-        exclude = [] if excluded is None else list(excluded)
-        readonly_fields = self.get_readonly_fields(request, obj)
-        exclude.extend(readonly_fields)
-        # Exclude all fields if it's a change form and the user doesn't have
-        # the change permission.
-        if (
-            change
-            and hasattr(request, "user")
-            and not self.has_change_permission(request, obj)
-        ):
-            exclude.extend(fields)
-        if excluded is None and hasattr(self.form, "_meta") and self.form._meta.exclude:
-            # Take the custom ModelForm's Meta.exclude into account only if the
-            # ModelAdmin doesn't define its own.
-            exclude.extend(self.form._meta.exclude)
-        # if exclude is an empty list we pass None to be consistent with the
-        # default on modelform_factory
-        exclude = exclude or None
-
-        # Remove declared form fields which are in readonly_fields.
-        new_attrs = dict.fromkeys(
-            f for f in readonly_fields if f in self.form.declared_fields
-        )
-        form = type(self.form.__name__, (self.form,), new_attrs)
-
-        defaults = {
-            "form": form,
-            "fields": fields,
-            "exclude": exclude,
-            "formfield_callback": partial(self.formfield_for_dbfield, request=request),
-            **kwargs,
-        }
-
-        if defaults["fields"] is None and not modelform_defines_fields(
-            defaults["form"]
-        ):
-            defaults["fields"] = forms.ALL_FIELDS
-
-        try:
-            return modelform_factory(self.model, **defaults)
-        except FieldError as e:
-            raise FieldError(
-                "%s. Check fields/fieldsets/exclude attributes of class %s."
-                % (e, self.__class__.__name__)
-            )
-
-
 @shared_task()
 def mass_edit(request, comma_separated_object_ids, app_name, model_name, serialized_queryset, mass_changes_fields, admin_site_name):
     """
@@ -132,8 +68,7 @@ def mass_edit(request, comma_separated_object_ids, app_name, model_name, seriali
     admin_model = admin.ModelAdmin(model, admin_site)
 
     ModelForm = admin_model.get_form(request, obj)
-
-    # try:
+    
     with transaction.atomic():
         objects_count = 0
         changed_count = 0
@@ -141,14 +76,14 @@ def mass_edit(request, comma_separated_object_ids, app_name, model_name, seriali
         for obj in objects:
             objects_count += 1
             form = ModelForm(
-                request["POST"],
-                request["FILES"],
+                request.POST,
+                request.FILES,
                 instance=obj)
 
             # refresh InMemoryUploadedFile object.
             # It should not cause memory leaks as it
             # only fseeks to the beggining of the media file.
-            for in_memory_file in request["FILES"].values():
+            for in_memory_file in request.FILES.values():
                 in_memory_file.open()
 
             exclude = []
@@ -161,27 +96,29 @@ def mass_edit(request, comma_separated_object_ids, app_name, model_name, seriali
 
             if form.is_valid():
                 form_validated = True
-                new_object = form.save()
+                new_object = admin_model.save_form(
+                    request,
+                    form,
+                    change=True)
             else:
                 form_validated = False
                 new_object = obj
             prefixes = {}
-
-            # Adds a prefix to all formsets
-            for FormSet in get_formsets(admin_model, request, new_object): #request
+            for FormSet in get_formsets(admin_model, request, new_object):
                 prefix = FormSet.get_default_prefix()
                 prefixes[prefix] = prefixes.get(prefix, 0) + 1
                 if prefixes[prefix] != 1:
                     prefix = "%s-%s" % (prefix, prefixes[prefix])
                 if prefix in mass_changes_fields:
                     formset = FormSet(
-                        request["POST"],
-                        request["FILES"],
+                        request.POST,
+                        request.FILES,
                         instance=new_object,
                         prefix=prefix)
                     formsets.append(formset)
 
             if all_valid(formsets) and form_validated:
+                # admin_model.admin_obj.save_model(request, new_object, form, change=True)
                 admin_model.save_model(
                     request,
                     new_object,
@@ -189,17 +126,97 @@ def mass_edit(request, comma_separated_object_ids, app_name, model_name, seriali
                     change=True)
                 form.save_m2m()
                 for formset in formsets:
-                    formset.save()
+                    admin_model.save_formset(
+                        request,
+                        form,
+                        formset,
+                        change=True)
+
+                change_message = admin_model.construct_change_message(
+                    request,
+                    form,
+                    formsets)
+                admin_model.log_change(
+                    request,
+                    new_object,
+                    change_message)
                 changed_count += 1
 
-        if changed_count != objects_count:
+        if changed_count == objects_count:
+            return admin_model.response_change(request, new_object)
+        else:
             errors = form.errors
             errors_list = helpers.AdminErrorList(form, formsets)
-            logger.error(errors)
-            logger.error(errors_list)
-
             # Raise error for rollback transaction in atomic block
             raise ValidationError("Not all forms is correct")
+
+    # # try:
+    # with transaction.atomic():
+    #     objects_count = 0
+    #     changed_count = 0
+    #     objects = queryset.filter(pk__in=object_ids)
+    #     for obj in objects:
+    #         objects_count += 1
+    #         form = ModelForm(
+    #             request["POST"],
+    #             request["FILES"],
+    #             instance=obj)
+
+    #         # refresh InMemoryUploadedFile object.
+    #         # It should not cause memory leaks as it
+    #         # only fseeks to the beggining of the media file.
+    #         for in_memory_file in request["FILES"].values():
+    #             in_memory_file.open()
+
+    #         exclude = []
+    #         for fieldname, field in list(form.fields.items()):
+    #             if fieldname not in mass_changes_fields:
+    #                 exclude.append(fieldname)
+
+    #         for exclude_fieldname in exclude:
+    #             del form.fields[exclude_fieldname]
+
+    #         if form.is_valid():
+    #             form_validated = True
+    #             new_object = form.save()
+    #         else:
+    #             form_validated = False
+    #             new_object = obj
+    #         prefixes = {}
+
+    #         # Adds a prefix to all formsets
+    #         for FormSet in get_formsets(admin_model, request, new_object): #request
+    #             prefix = FormSet.get_default_prefix()
+    #             prefixes[prefix] = prefixes.get(prefix, 0) + 1
+    #             if prefixes[prefix] != 1:
+    #                 prefix = "%s-%s" % (prefix, prefixes[prefix])
+    #             if prefix in mass_changes_fields:
+    #                 formset = FormSet(
+    #                     request["POST"],
+    #                     request["FILES"],
+    #                     instance=new_object,
+    #                     prefix=prefix)
+    #                 formsets.append(formset)
+
+    #         if all_valid(formsets) and form_validated:
+    #             admin_model.save_model(
+    #                 request,
+    #                 new_object,
+    #                 form,
+    #                 change=True)
+    #             form.save_m2m()
+    #             for formset in formsets:
+    #                 formset.save()
+    #             changed_count += 1
+
+    #     if changed_count != objects_count:
+    #         errors = form.errors
+    #         errors_list = helpers.AdminErrorList(form, formsets)
+    #         logger.error(errors)
+    #         logger.error(errors_list)
+
+    #         # Raise error for rollback transaction in atomic block
+    #         raise ValidationError("Not all forms is correct")
 
     # except Exception as e:
     #     logger.error(e)
