68cbe8c7c134036f2c4e1a93f92cd820f5b77db1
Sebastian Wroblewski <swroblewski@motoinsight.com>
2022-11-30 14:33:34 +0100

    MI-1415: Version bump dev24

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="autosync-django-mass-edit",
-    version="3.4.1b4.dev23",
+    version="3.4.1b4.dev24",
     author="David Burke",
     author_email="david@burkesoftware.com",
     description=("Make bulk changes in the Django admin interface"),
